54a64f1a0a7272ccc1e376bbd5874b937818ed60
otakushimmps <otakushimmps@gmail.com>
2025-12-18 16:18:15 +0800

    fix: use correct GH Pages base

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/tammystudio_PregnantWax_Quiz/',
   plugins: [react()],
 });
